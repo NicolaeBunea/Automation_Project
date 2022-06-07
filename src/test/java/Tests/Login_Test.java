4be2e073df6a51e83e807cf2bbb68d096dd740cd
NicolaeBunea <nicolae_bunea23@yahoo.com>
2022-06-07 19:32:04 +0300

    finish login test

--- src/test/java/Tests/Login_Test.java
+++ src/test/java/Tests/Login_Test.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,6 +24,10 @@ public class Login_Test {
         //Accesam un URL
         Driver.get("http://demo.automationtesting.in/Index.html");
 
+        //Maximize page
+
+        Driver.manage().window().maximize();
+
         //Identificam butonul "Sign In".. din inspect si luam ID-ul
 
         //ID, Class, Link, Value, XPATH, CSS
@@ -31,6 +36,12 @@ public class Login_Test {
 
         WebElement signInElement= Driver.findElement(By.id("btn1"));
         signInElement.click();
+
+        //Validam pagina de login
+
+        String expectedPage="SignIn";
+        String actualPage= Driver.getTitle();
+        Assert.assertEquals("The expected page was not displayed",expectedPage,actualPage);
 
         //Identificam butonul de email
 
@@ -48,6 +59,23 @@ public class Login_Test {
 
         WebElement enterElement= Driver.findElement(By.id("enterbtn"));
         enterElement.click();
+
+        //Validam mesaj de eroare
+
+        WebElement mesageElement= Driver.findElement(By.id("errormsg"));
+        String expectedError= "Invalid User Name or PassWord";
+        String actualError= mesageElement.getText();
+        Assert.assertEquals("Text of the error displayed is not correct",expectedError,actualError);
+
+        //Inchidem paginile
+
+        /*Driver.close();*/
+        Driver.quit();
+
+        //Dif intre close si quit: "close" inchide cate un tab iar "quit" browserul
+
+
+
 
 
 
